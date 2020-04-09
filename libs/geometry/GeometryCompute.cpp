fa033c368188cc915e85e1f8f4011ad75d818895
zjk <knd@knd.com.cn>
2020-04-09 10:38:34 +0800

    remove uunused code; close gcc stack over check; fix degree = 8 quit

--- libs/geometry/GeometryCompute.cpp
+++ libs/geometry/GeometryCompute.cpp
@@ -16,16 +16,20 @@ int PointArray::AddPoint(Point &Point)
     }
 }
 
-void Point::MidPoint(const Point &Pt1, const Point &Pt2)
-{
-    x = (Pt1.x + Pt2.x)/2;
-    y = (Pt1.y + Pt2.y)/2;
-    z = (Pt1.z + Pt2.z)/2;
-}
-
+/**
+ * @brief GeometryCompute::DistanceToLine
+ * @param Pt 输入: 直线外的点
+ * @param LineP1 输入: 直线上的点P1
+ * @param LineP2 输入: 直线上的点P2
+ * @param Dim 输入: 点的维数
+ * @param Distance 输出: 点Pt到直线P1P2的距离的平方
+ * @return 返回操作结果状态码 ERRCODE
+ *  算法：假设Pt与直线P1P2的垂线的交点为Pc，先求系数 t=|PcP1|/|P1P2|, 然后求Pc坐标，
+ *     最后求|PCP1|
+ */
 double Point::DistanceToLine(const Point &Pt,
-                          const Point &P1,
-                          const Point &P2)
+                             const Point &P1,
+                             const Point &P2)
 {
     Point P1P2;
     Point P1Pt;
@@ -65,128 +69,6 @@ void GeometryCompute::SetTolerance(double tolerance)
     mDistanceTolerance = mDistanceTolerance < MIN_TOLERANCE*MIN_TOLERANCE ? MIN_TOLERANCE*MIN_TOLERANCE: tolerance;
 }
 
-int GeometryCompute::InterpolateBezier(ControlPoints ControlPts, PointArray &PtOut)
-{
-    int Ret = SUCCESS;
-
-    PtOut.validIndex = 0;
-    PtOut.AddPoint(ControlPts.points[0]);
-
-    if (ControlPts.degree == 2)
-    {
-//		Ret = Recursive2DegreeBezier(ControlPts.points[0], ControlPts.points[2], ControlPts.points[4], 0, PtOut);
-    }
-    else if (ControlPts.degree == 3)
-    {
-        Ret = Recursive3DegreeBezier(ControlPts.points[0], ControlPts.points[1], ControlPts.points[2], ControlPts.points[3],
-                0, PtOut);
-    } else {
-        return DEGREE_INVALID;
-    }
-
-    if (Ret == SUCCESS)
-    {
-        PtOut.AddPoint(ControlPts.points[ControlPts.degree]);
-    }
-
-    return Ret;
-}
-
-/**
- * @brief GeometryCompute::DistanceToLine
- * @param Pt 输入: 直线外的点
- * @param LineP1 输入: 直线上的点P1
- * @param LineP2 输入: 直线上的点P2
- * @param Dim 输入: 点的维数
- * @param Distance 输出: 点Pt到直线P1P2的距离的平方
- * @return 返回操作结果状态码 ERRCODE
- *  算法：假设Pt与直线P1P2的垂线的交点为Pc，先求系数 t=|PcP1|/|P1P2|, 然后求Pc坐标，
- *     最后求|PCP1|
- */
-int GeometryCompute::DistanceToLine(const double Pt[], const double P1[], const double P2[], int Dim,
-                                    double *Distance)
-{
-    double P1P2[MAX_DIMENSION] = {0.0};
-    double P1Pt[MAX_DIMENSION] = {0.0};
-    double P1P2Dist = 0.0;   // P1P2线段长度的平方
-    double T = 0.0;  // P1Pt 点积 P1P2
-    double tmp = 0.0;
-
-    if (Dim > MAX_DIMENSION)
-    {
-        return DIM_INVALID;
-    }
-
-    for (int i = 0; i < Dim; ++i)
-    {
-        P1P2[i] = P2[i] - P1[i];
-        P1Pt[i] = Pt[i] - P1[i];
-    }
-
-    for (int i = 0; i < Dim; ++i)
-    {
-        P1P2Dist += P1P2[i]*P1P2[i];
-        T += P1P2[i]*P1Pt[i];
-    }
-
-    if (P1P2Dist < 0.001)
-    {
-        return PARA_INVALID;
-    }
-
-    T /= P1P2Dist;  // 系数t
-
-    for (int i = 0; i < Dim; ++i)
-    {
-        tmp = P1[i] + T*P1P2[i] - Pt[i]; // PtPc坐标分量差, 其中Pc坐标为LineP1[i] + T*P1P2[i]
-        *Distance += tmp*tmp;
-    }
-
-    return SUCCESS;
-}
-
-int GeometryCompute::InterpolateBezier(double BezierControlPts[], int Degree, int Dim,
-                                       double PtOut[], int Size, int *UsedSize)
-{
-    int Ret = SUCCESS;
-    mPtMem = PtOut;
-    mPtTotalSize = Size;
-    mPtIndex = 0;
-    if (Degree < 2 || Degree > 3) {
-        return DEGREE_INVALID;
-    }
-
-    Ret = AddPoint(&BezierControlPts[0], Dim);
-    if (Ret != SUCCESS)
-    {
-        return NOT_ENOUGH_MEMERY;
-    }
-
-    if (Degree == 2)
-    {
-//		Ret = Recursive2DegreeBezier(&BezierControlPts[0], &BezierControlPts[2], &BezierControlPts[4], 0);
-    }
-    else
-    {
-        Ret = Recursive3DegreeBezier(&BezierControlPts[0*Dim], &BezierControlPts[1*Dim], &BezierControlPts[2*Dim], &BezierControlPts[3*Dim], Dim, 0);
-    }
-
-    if (Ret != SUCCESS)
-    {
-        return Ret;
-    }
-
-    Ret = AddPoint(&BezierControlPts[Degree*Dim], Dim);
-    if (Ret != SUCCESS)
-    {
-        return NOT_ENOUGH_MEMERY;
-    }
-
-    *UsedSize = mPtIndex * sizeof(double);
-
-    return SUCCESS;
-}
-
 int GeometryCompute::DecomposeNurbsToLine(const Nurbs &Nurbs, PointArray &PtOut)
 {
     int Ret = SUCCESS;
@@ -205,8 +87,8 @@ int GeometryCompute::DecomposeNurbsToLine(const Nurbs &Nurbs, PointArray &PtOut)
     }
 
     PtOut.validIndex = 0;
-    Point Qw[MAX_DEGREE];
-    Point QwNext[MAX_DEGREE];
+    Point Qw[MAX_DEGREE+1];
+    Point QwNext[MAX_DEGREE+1];
 
     for (int i = 0; i <= p; ++i)
     {
@@ -311,8 +193,8 @@ int GeometryCompute::RecursiveBezier(Point Pts[], int Degree, int Level, PointAr
     int Ret = SUCCESS;
     double DisMax = 0.0;
     double Tmp = 0.0;
-    Point PtMid1[MAX_DEGREE];
-    Point PtMid2[MAX_DEGREE];
+    Point PtMid1[MAX_DEGREE+1];
+    Point PtMid2[MAX_DEGREE+1];
 
     if(Level > CURVE_RECURSION_LIMIT)
     {
@@ -441,135 +323,6 @@ int GeometryCompute::DecomposeCurve(int n, int p, const double *U, const Point *
             a = b;
             b++;
         }
-    }
-
-    return SUCCESS;
-}
-
-//int GeometryCompute::Recursive2DegreeBezier(double Pt1[3], double Pt2[3], double Pt3[3], int Level)
-//{
-//	return SUCCESS;
-//}
-
-void GeometryCompute::MidPoint(double P1[], double P2[], double PMid[], int Dim)
-{
-    for (int i = 0; i < Dim; ++i)
-    {
-        PMid[i] = (P1[i] + P2[i]) / 2;
-    }
-}
-
-int GeometryCompute::Recursive3DegreeBezier(double Pt1[],
-                                            double Pt2[],
-                                            double Pt3[],
-                                            double Pt4[],
-                                            int Dim,
-                                            int Level)
-{
-    int Ret = SUCCESS;
-    double Pt12[MAX_DIMENSION] = {0.0};
-    double Pt23[MAX_DIMENSION] = {0.0};
-    double Pt34[MAX_DIMENSION] = {0.0};
-    double Pt123[MAX_DIMENSION] = {0.0};
-    double Pt234[MAX_DIMENSION] = {0.0};
-    double Pt1234[MAX_DIMENSION] = {0.0};
-
-    double Dis12 = {0.0};
-    double Dis23 = {0.0};
-
-    if(Level > CURVE_RECURSION_LIMIT)
-    {
-        return RECURSIVE_OVERFLOW;
-    }
-
-    // Calculate all the mid-points of the line segments
-    MidPoint(Pt1, Pt2, Pt12, Dim);
-    MidPoint(Pt2, Pt3, Pt23, Dim);
-    MidPoint(Pt3, Pt4, Pt34, Dim);
-    MidPoint(Pt12, Pt23, Pt123, Dim);
-    MidPoint(Pt23, Pt34, Pt234, Dim);
-    MidPoint(Pt123, Pt234, Pt1234, Dim);
-
-    if (Level > 0) {
-        // Try to approximate the full cubic curve by a single straight line
-        DistanceToLine(Pt2, Pt1, Pt4, Dim, &Dis12); // ToDo
-        DistanceToLine(Pt3, Pt1, Pt4, Dim, &Dis23);
-
-        if(Dis12 + Dis23 < mDistanceTolerance)
-        {
-            return AddPoint(Pt1234, Dim);
-        }
-    }
-
-    // Continue subdivision
-    Ret = Recursive3DegreeBezier(Pt1, Pt12, Pt123, Pt1234, Dim, Level+1);
-    if (Ret != SUCCESS)
-    {
-        return Ret;
-    }
-
-    Ret = Recursive3DegreeBezier(Pt1234, Pt234, Pt34, Pt4, Dim, Level+1);
-    if (Ret != SUCCESS)
-    {
-        return Ret;
-    }
-
-    return SUCCESS;
-}
-
-//int GeometryCompute::Recursive2DegreeBezier(Point Pt1, Point Pt2, Point Pt3, int Level, PointArray &PtOut)
-//{
-//	return SUCCESS;
-//}
-
-int GeometryCompute::Recursive3DegreeBezier(Point Pt1, Point Pt2, Point Pt3, Point Pt4, int Level, PointArray &PtOut)
-{
-    int Ret = SUCCESS;
-    Point Pt12, Pt23, Pt34;
-    Point Pt123, Pt234;
-    Point Pt1234;
-
-    double Dis12 = {0.0};
-    double Dis23 = {0.0};
-
-    if(Level > CURVE_RECURSION_LIMIT)
-    {
-        return RECURSIVE_OVERFLOW;
-    }
-
-    // Calculate all the mid-points of the line segments
-    Pt12.MidPoint(Pt1, Pt2);
-    Pt23.MidPoint(Pt2, Pt3);
-    Pt34.MidPoint(Pt3, Pt4);
-
-    Pt123.MidPoint(Pt12, Pt23);
-    Pt234.MidPoint(Pt23, Pt34);
-
-    Pt1234.MidPoint(Pt123, Pt234);
-
-    if (Level > 0) {
-        // Try to approximate the full cubic curve by a single straight line
-        Dis12 = Point::DistanceToLine(Pt2, Pt1, Pt4);
-        Dis23 = Point::DistanceToLine(Pt3, Pt1, Pt4);
-
-        if(Dis12 + Dis23 < mDistanceTolerance)
-        {
-            PtOut.AddPoint(Pt1234);
-            return SUCCESS;
-        }
-    }
-
-    // Continue subdivision
-    Ret = Recursive3DegreeBezier(Pt1, Pt12, Pt123, Pt1234, Level+1, PtOut);
-    if (Ret != SUCCESS)
-    {
-        return Ret;
-    }
-
-    Ret = Recursive3DegreeBezier(Pt1234, Pt234, Pt34, Pt4, Level+1, PtOut);
-    if (Ret != SUCCESS)
-    {
-        return Ret;
     }
 
     return SUCCESS;
--- libs/geometry/GeometryCompute.h
+++ libs/geometry/GeometryCompute.h
@@ -1,30 +1,29 @@
 #ifndef GEOMETRYCOMPUTE_H
 #define GEOMETRYCOMPUTE_H
 
-#include <float.h>
-
-struct Point {
-    Point(): x(0.0), y(0.0), z(0.0), w(1.0) {}
+struct Point
+{
+    Point(): x(0.0), y(0.0), z(0.0), w(0.0) {}
     Point(double x, double y, double z, double w = 1.0): x(x), y(y), z(z), w(w) {}
     double x, y, z, w;
     inline Point &operator+=(const Point &p)
     {
-        x+=p.x; y+=p.y; z+=p.z; return *this;
+        x+=p.x; y+=p.y; z+=p.z; w+=p.w; return *this;
     }
 
     inline Point &operator-=(const Point &p)
     {
-        x-=p.x; y-=p.y; z-=p.z; return *this;
+        x-=p.x; y-=p.y; z-=p.z; w-=p.w; return *this;
     }
 
     inline Point &operator*=(double c)
     {
-        x*=c; y*=c; z*=c; return *this;
+        x*=c; y*=c; z*=c; w*=c; return *this;
     }
 
     inline Point &operator/=(double c)
     {
-        x/=c; y/=c; z/=c; return *this;
+        x/=c; y/=c; z/=c; w/=c; return *this;
     }
 
     friend inline const Point operator+(const Point &p)
@@ -34,36 +33,36 @@ struct Point {
 
     friend inline const Point operator-(const Point &p)
     {
-        return Point(-p.x, -p.y, -p.z);
+        return Point(-p.x, -p.y, -p.z, -p.w);
     }
 
     friend inline const Point operator+(const Point &p1, const Point &p2)
     {
-        return Point(p1.x+p2.x, p1.y+p2.y, p1.z+p2.z);
+        return Point(p1.x+p2.x, p1.y+p2.y, p1.z+p2.z, p1.w+p2.w);
     }
 
     friend inline const Point operator-(const Point &p1, const Point &p2)
     {
-        return Point(p1.x-p2.x, p1.y-p2.y, p1.z-p2.z);
+        return Point(p1.x-p2.x, p1.y-p2.y, p1.z-p2.z, p1.w-p2.w);
     }
 
     friend inline const Point operator*(const Point &p, double c)
     {
-        return Point(p.x*c, p.y*c, p.z*c);
+        return Point(p.x*c, p.y*c, p.z*c, p.w*c);
     }
 
     friend inline const Point operator*(double c, const Point &p)
     {
-        return Point(p.x*c, p.y*c, p.z*c);
+        return Point(p.x*c, p.y*c, p.z*c, p.w*c);
     }
 
     friend inline const Point operator/(const Point &p, double divisor)
     {
-        return Point(p.x/divisor, p.y/divisor, p.z/divisor);
+        return Point(p.x/divisor, p.y/divisor, p.z/divisor, p.w/divisor);
     }
 
     static inline double dotProduct(const Point &p1, const Point &p2)
-    { return p1.x*p2.x + p1.y*p2.y + p1.z*p2.z; }
+    { return p1.x*p2.x + p1.y*p2.y + p1.z*p2.z + p1.w*p2.w; }
 
 
     static inline bool isNull(double d)
@@ -82,7 +81,6 @@ struct Point {
         return false;
     }
 
-    void MidPoint(const Point &Pt1, const Point &Pt2);
     static double DistanceToLine(const Point &Pt, const Point &P1, const Point &P2);
 };
 
@@ -137,23 +135,11 @@ public:
     double GetMinTolerance() { return MIN_TOLERANCE; }
 
     int DecomposeNurbsToLine(const Nurbs &Nurbs, PointArray &PtOut);
-
     int DecomposeCurve(int n, int p, const double *U, const Point *Pw, int &nb, Point *Qw);
-    int InterpolateBezier(ControlPoints ControlPts, PointArray &PtOut);
-    int InterpolateBezier(double BezierControlPts[], int Degree, int Dim,
-                          double PtOut[], int Size, int *UsedSize);
+
 private:
     int RecursiveBezier(Point Pts[], int Degree, int Level, PointArray &PtOut);
-
-    int Recursive3DegreeBezier(double Pt1[], double Pt2[], double Pt3[], double Pt4[],
-                               int Dim, int Level);
-
-    int Recursive3DegreeBezier(Point Pt1, Point Pt2, Point Pt3, Point Pt4, int Level, PointArray &PtOut);
-
-    static int DistanceToLine(const double Pt[], const double P1[], const double P2[], int Dim,
-                              double *Distance);
     int AddPoint(double Pt[], int Dim);
-    static void MidPoint(double P1[], double P2[], double PMid[], int Dim);
     int InterpolateBezier(int Degree, Point *ControlPts, PointArray &PtOut);
 
     int CURVE_RECURSION_LIMIT;
